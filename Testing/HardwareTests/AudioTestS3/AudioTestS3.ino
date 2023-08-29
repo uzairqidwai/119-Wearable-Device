34537f78e6e7dd716b7fd023177478cb0d60afac
Uzair Qidwai <uzairqidwai1@gmail.com>
2023-08-29 13:16:17 -0400

    update

--- Testing/HardwareTests/AudioTestS3/AudioTestS3.ino
+++ Testing/HardwareTests/AudioTestS3/AudioTestS3.ino
@@ -1,52 +1,58 @@
-#include <Arduino.h>
 #include <driver/i2s.h>
+#include <math.h>
 
-#define I2S_NUM         (1)   // i2s port number
-#define WAVE_FREQ_HZ    (100) // sound frequency
-#define SAMPLE_RATE     (36000)
-#define I2S_BCK_PIN     (43)
-#define I2S_WS_PIN      (44)
-#define I2S_DATA_PIN    (21)
+#define SAMPLE_RATE 44100
+#define PIN_I2S_BCLK 3
+#define PIN_I2S_LRC 10
+#define PIN_I2S_DIN 11
+#define PI 3.14159265
 
 void setup() {
   i2s_config_t i2s_config = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-    .sample_rate = SAMPLE_RATE,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-    .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 8,
-    .dma_buf_len = 64
+    mode: (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+    sample_rate: SAMPLE_RATE,
+    bits_per_sample: I2S_BITS_PER_SAMPLE_16BIT,
+    channel_format: I2S_CHANNEL_FMT_RIGHT_LEFT,
+    communication_format: I2S_COMM_FORMAT_I2S,
+    intr_alloc_flags: ESP_INTR_FLAG_LEVEL1,
+    dma_buf_count: 8,
+    dma_buf_len: 64,
+    use_apll: false
   };
 
   i2s_pin_config_t pin_config = {
-    .bck_io_num = I2S_BCK_PIN,
-    .ws_io_num = I2S_WS_PIN,
-    .data_out_num = I2S_DATA_PIN,
-    .data_in_num = I2S_PIN_NO_CHANGE
+    bck_io_num: PIN_I2S_BCLK,
+    ws_io_num: PIN_I2S_LRC,
+    data_out_num: PIN_I2S_DIN,
+    data_in_num: I2S_PIN_NO_CHANGE
   };
 
-  i2s_driver_install((i2s_port_t)I2S_NUM, &i2s_config, 0, NULL);
-  i2s_set_pin((i2s_port_t)I2S_NUM, &pin_config);
-  i2s_set_clk((i2s_port_t)I2S_NUM, SAMPLE_RATE, (i2s_bits_per_sample_t)I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
-
+  i2s_driver_install((i2s_port_t)I2S_NUM_0, &i2s_config, 0, NULL);
+  i2s_set_pin((i2s_port_t)I2S_NUM_0, &pin_config);
 }
 
 void loop() {
+  int low_frequency = 440; // Hz
+  int high_frequency = 880; // Hz
+  int frequency = low_frequency;
+  int samples_per_wave = SAMPLE_RATE / frequency;
   int16_t sample;
-  float t = 0;
-  int16_t amplitude = 10000; // adjust this value to change the volume
-  while (1) {
-    for (int i = 0; i < SAMPLE_RATE / WAVE_FREQ_HZ; i++) {
-      sample = (int16_t)(amplitude * sin(t));
-      i2s_write_sample(sample);
-      t += TWO_PI * WAVE_FREQ_HZ / SAMPLE_RATE;
+  size_t bytes_written;
+  float radian_increment = 2.0 * PI / samples_per_wave;
+  float volume = 1.0; // 100%
+
+  for (int i = 0; i < SAMPLE_RATE; i++) {
+    float radian = radian_increment * i;
+    sample = (int16_t)(sin(radian) * 0x7FFF * volume);
+
+    i2s_write(I2S_NUM_0, &sample, 2, &bytes_written, portMAX_DELAY);
+    i2s_write(I2S_NUM_0, &sample, 2, &bytes_written, portMAX_DELAY);
+    
+    // change frequency to create wailing siren sound
+    if (i % (SAMPLE_RATE / 2) == 0) {
+      frequency = (frequency == low_frequency) ? high_frequency : low_frequency;
+      samples_per_wave = SAMPLE_RATE / frequency;
+      radian_increment = 2.0 * PI / samples_per_wave;
     }
   }
-}
-
-void i2s_write_sample(int16_t sample) {
-  size_t bytes_written;
-  i2s_write((i2s_port_t)I2S_NUM, &sample, sizeof(sample), &bytes_written, portMAX_DELAY);
 }
